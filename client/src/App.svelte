3f4d37c1d8a8c2595e94f2fbbffceea407f321c2
git-shru <mca.1904@unigoa.ac.in>
2021-03-28 16:13:04 +0530

    navbar

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -6,10 +6,37 @@
 
 <main>
   <Router>
-  <nav class="pt-2 pb-2">
-    nav
-    <!-- @S add nav items (use Link) -->
-  </nav>
+ 
+  <nav class="navbar navbar-inverse navb">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">♡ Doggo ♡</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Adopt</a></li>
+        <li><a href="#">Foster</a></li>
+        <li><a href="#">Volunteer</a></li>
+        <li><a href="#">Events</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Help<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Pet food</a></li>
+            <li><a href="#">Vaccinaton Center</a></li>
+            
+          </ul>
+        </li>
+      </ul>
+    
+    </div>
+  </div>
+</nav>
     <!-- add routes -->
     <Route path="/"><Home /></Route>
   </Router>
@@ -25,4 +52,10 @@
 	@media (min-width: 640px) {
 		
 	}
+
+
+.navb{
+	background-color:#302121;
+}
+
 </style>
