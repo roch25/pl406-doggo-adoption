61b58778f93aebc067c97da9af29fcd4d77c89ec
git-shru <mca.1904@unigoa.ac.in>
2021-03-29 22:51:09 +0530

    navbar

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,41 +2,68 @@
 	import { Router, Route, Link } from "svelte-routing";
 	import Home from './components/Home.svelte'
 	import Footer from './components/Footer.svelte'
+
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+
+
 </script>
 
 <main>
   <Router>
  
-  <nav class="navbar navbar-inverse navb">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">♡ Doggo ♡</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Adopt</a></li>
-        <li><a href="#">Foster</a></li>
-        <li><a href="#">Volunteer</a></li>
-        <li><a href="#">Events</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Help<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Pet food</a></li>
-            <li><a href="#">Vaccinaton Center</a></li>
-            
-          </ul>
-        </li>
-      </ul>
-    
-    </div>
-  </div>
-</nav>
+ <nav>
+
+<Navbar color=" " dark expand="md">
+  <NavbarBrand href="/">Doggo ♡</NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ml-auto" navbar>
+      <NavItem>
+        <NavLink href="#Home/">Home</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#Adopt/">Adopt</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#Foster/">Foster</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="Volunteer">Volunteer</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="Event">Event</NavLink>
+      </NavItem>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>Help</DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem>Pet food</DropdownItem>
+          <DropdownItem>Vaccination Centre</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
+
+ </nav>
+
     <!-- add routes -->
     <Route path="/"><Home /></Route>
   </Router>
@@ -53,9 +80,8 @@
 		
 	}
 
+	nav{
+	  background-color:#302121;
 
-.navb{
-	background-color:#302121;
-}
-
+	}
 </style>
